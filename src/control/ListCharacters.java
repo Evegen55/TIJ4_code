a0903d11be5f9ae50f4ee624f5be24a1ac06e4e8
Evegenn <demerbrise@gmail.com>
2016-04-04 16:21:09 +0300

    add library org.eclipse.swt.widgets for windows see more: http://archive.eclipse.org/eclipse/downloads/drops4/R-4.4.2-201502041700/index.php#SWT

--- src/control/ListCharacters.java
+++ src/control/ListCharacters.java
@@ -7,7 +7,7 @@ package control;
 public class ListCharacters {
   public static void main(String[] args) {
     for(char c = 0; c < 128; c++)
-      if(Character.isLowerCase(c))
+     // if(Character.isLowerCase(c))
         System.out.println("value: " + (int)c +
           " character: " + c);
   }
